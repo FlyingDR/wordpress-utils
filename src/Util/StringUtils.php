9413c8f6db40f7a41d1124c110fda7fe5082424c
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2018-11-24 12:58:17 +0300

    Added support for generating slugs using third-party slugify library in a case if there is no native transliteration support in iconv

--- src/Util/StringUtils.php
+++ src/Util/StringUtils.php
@@ -2,11 +2,22 @@
 
 namespace Flying\Wordpress\Util;
 
+use Cocur\Slugify\Slugify;
+
 /**
  * Various utility functions that are related to strings processing
  */
 class StringUtils
 {
+    /**
+     * @var string
+     */
+    private static $slugMethod;
+    /**
+     * @var Slugify
+     */
+    private static $slugify;
+
     /**
      * Convert given string into slug-alike string
      *
@@ -15,7 +26,24 @@ class StringUtils
      */
     public static function toSlug($string)
     {
-        /** @noinspection PhpComposerExtensionStubsInspection */
-        return strtolower(preg_replace('/\s+/', '-', trim(iconv('utf-8', 'us-ascii//TRANSLIT', $string))));
+        if (!self::$slugMethod) {
+            if (iconv('utf-8', 'us-ascii//TRANSLIT', 'ā') === 'a') {
+                self::$slugMethod = 'iconv';
+            } elseif (class_exists(Slugify::class)) {
+                self::$slugMethod = 'slugify';
+                self::$slugify = new Slugify();
+            } else {
+                throw new \RuntimeException('There is no available method for generating slugs, consider installing cocur/slugify library');
+            }
+        }
+        switch (self::$slugMethod) {
+            case 'iconv':
+                return strtolower(preg_replace('/\s+/', '-', trim(iconv('utf-8', 'us-ascii//TRANSLIT', $string))));
+                break;
+            case 'slugify':
+                return self::$slugify->slugify($string);
+                break;
+        }
+        return $string;
     }
 }
